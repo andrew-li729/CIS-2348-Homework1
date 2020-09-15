59c747c8a02861912f3e6873d4851c6a797cd561
Andrew <andrew.li729@gmail.com>
2020-09-15 03:19:08 -0500

    added code

---
+++ Lab_1.20.py
@@ -0,0 +1,18 @@
+# Andrew Li
+# 1824794
+
+print("Enter integer:")
+user_num1 = int(input())
+print("You entered: ", user_num1)
+
+squared = user_num1 * user_num1
+cubed = user_num1 * user_num1 * user_num1
+print(user_num1, "squared is", squared)
+print("And", user_num1, "cubed is", cubed, "!!")
+
+print("Enter another integer:")
+user_num2 = int(input())
+sum = user_num1 + user_num2
+product = user_num1 * user_num2
+print("{} + {} is {}".format(user_num1, user_num2, sum))
+print("{} * {} is {}".format(user_num1, user_num2, product))
