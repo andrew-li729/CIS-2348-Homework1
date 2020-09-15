5e279085534600ffe71678c65071e38f39303c66
Andrew <andrew.li729@gmail.com>
2020-09-15 15:34:38 -0500

    part one complete

---
+++ Lab_5.19.py
@@ -0,0 +1,17 @@
+# Andrew Li
+# 1824794
+
+# PART 1
+# Services menu
+services = {
+    "Oil change": 35,
+    "Tire rotation": 19,
+    "Car wash": 7,
+    "Car wax": 12
+}
+print("Davy's auto shop services")
+print("Oil change -- ${}".format(services["Oil change"]))
+print("Tire rotation -- ${}".format(services["Tire rotation"]))
+print("Car wash -- ${}".format(services["Car wash"]))
+print("Car wax -- ${}".format(services["Car wax"]))
+
