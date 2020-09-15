607fa601356d1bd19e2af815ad214fe7552ae51c
Andrew <andrew.li729@gmail.com>
2020-09-15 14:28:54 -0500

    fixed issues to receive all points for part 1

--- Lab_2.19.py
+++ Lab_2.19.py
@@ -1,12 +1,17 @@
 # Andrew Li
 # 1824794
 
+# PART 1
 lemon_cups = float(input("Enter amount of lemon juice (in cups):\n"))
 water_cups = float(input("Enter amount of water (in cups):\n"))
 agave_cups = float(input("Enter amount of agave nectar (in cups):\n"))
 num_servings = float(input("How many servings does this make?\n"))
-print("\n")
+print("")
 print("Lemonade ingredients - yields {:.2f} servings".format(num_servings))
 print("{:.2f} cup(s) lemon juice".format(lemon_cups))
 print("{:.2f} cup(s) water".format(water_cups))
-print("{:.2f} cup(s) agave nectar".format(agave_cups))
+print("{:.2f} cup(s) agave nectar\n".format(agave_cups))
+
+
+
+
