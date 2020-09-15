2dec465624343e70b1dcc077481efe730c960b9a
Andrew <andrew.li729@gmail.com>
2020-09-15 14:18:58 -0500

    part 1 completed

--- Lab_2.19.py
+++ Lab_2.19.py
@@ -4,4 +4,9 @@
 lemon_cups = float(input("Enter amount of lemon juice (in cups):"))
 water_cups = float(input("Enter amount of water (in cups):"))
 agave_cups = float(input("Enter amount of agave nectar (in cups):"))
-num_servings =float(input("How many servings does this make?"))
+num_servings = float(input("How many servings does this make?\n"))
+
+print("Lemonade ingredients - yields {:.2f} servings".format(num_servings))
+print("{:.2f} cups(s) lemon juice".format(lemon_cups))
+print("{:.2f} cups(s) water".format(water_cups))
+print("{:.2f} cups(s) agave nectar".format(agave_cups))
