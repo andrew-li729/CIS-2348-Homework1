50e2e2678f0f4d53a62cb9033f6f8d8cf30f53df
Andrew <andrew.li729@gmail.com>
2020-09-15 15:03:37 -0500

    part two completed

--- Lab_3.18.py
+++ Lab_3.18.py
@@ -3,10 +3,13 @@
 
 # PART 1
 # Prompt user to enter height and width
-height = int(input("Enter wall height (feet)"))
-width = int(input("Enter wall width (feet)"))
+height = int(input("Enter wall height (feet):\n"))
+width = int(input("Enter wall width (feet):\n"))
 # calculate area
 area = int(height*width)
+print("Wall area: {} square feet\n".format(area))
 
-print("Wall area: {} square feet".format(area))
-
+# PART 2
+# Calculate gallons needed to paint wall
+gallons_paint = area/350  # one gallon of paint covers 350 sq ft
+print("Paint needed: {:.2f} gallons".format(gallons_paint))
