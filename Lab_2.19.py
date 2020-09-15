ff6fb231f889ec3ff03d1f1f4366a2afdc9e8025
Andrew <andrew.li729@gmail.com>
2020-09-15 14:41:47 -0500

    formatted correctly for part two

--- Lab_2.19.py
+++ Lab_2.19.py
@@ -14,8 +14,8 @@ print("{:.2f} cup(s) agave nectar\n".format(agave_cups))
 
 # PART 2
 desired_servings = float(input("How many servings would you like to make?\n"))
+print("")
 servings_factor = desired_servings/num_servings  # every cup measurement will be multiplied by this factor
-
 lemon_cups = lemon_cups*servings_factor  # scales up servings by factor determined before
 water_cups = water_cups*servings_factor
 agave_cups = agave_cups*servings_factor
