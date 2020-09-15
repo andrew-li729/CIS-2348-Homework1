d9d97a991201aeec84ca247fbd20d528e02966a6
Andrew <andrew.li729@gmail.com>
2020-09-15 14:56:54 -0500

    added area calculation

---
+++ Lab_3.18.py
@@ -0,0 +1,12 @@
+# Andrew Li
+# 1824794
+
+# PART 1
+# Prompt user to enter height and width
+height = int(input("Enter wall height (feet)"))
+width = int(input("Enter wall width (feet)"))
+# calculate area
+area = int(height*width)
+
+print("Wall area: {} square feet".format(area))
+
