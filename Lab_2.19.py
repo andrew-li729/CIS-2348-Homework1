adc05ee2dc00a70320e83b34bd2c91c8807ca388
Andrew <andrew.li729@gmail.com>
2020-09-15 14:13:17 -0500

    initialize variable inputs

---
+++ Lab_2.19.py
@@ -0,0 +1,7 @@
+# Andrew Li
+# 1824794
+
+lemon_cups = float(input("Enter amount of lemon juice (in cups):"))
+water_cups = float(input("Enter amount of water (in cups):"))
+agave_cups = float(input("Enter amount of agave nectar (in cups):"))
+num_servings =float(input("How many servings does this make?"))
