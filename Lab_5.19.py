1aa882868759710a1b5bcf4b7a6570336652b50f
Andrew <andrew.li729@gmail.com>
2020-09-15 15:49:37 -0500

    part three complete

--- Lab_5.19.py
+++ Lab_5.19.py
@@ -21,4 +21,14 @@ print("Car wax -- ${}\n".format(services["Car wax"]))
 # will store choices in own variables to index dictionary
 service1 = input("Select first service:\n")
 service2 = input("Select second service:\n")
+print("")
 
+# PART 3
+service1_cost = services[service1]
+service2_cost = services[service2]
+total = service1_cost + service2_cost
+
+print("Davy's auto shop invoice\n")
+print("Service 1: {}, ${}".format(service1,services[service1]))
+print("Service 2: {}, ${}\n".format(service2,services[service2]))
+print("Total: $", total)
