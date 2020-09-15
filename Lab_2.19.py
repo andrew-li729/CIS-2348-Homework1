08437a59480859d52960dc83c0656acb212073ec
Andrew <andrew.li729@gmail.com>
2020-09-15 14:21:37 -0500

    formatting changes

--- Lab_2.19.py
+++ Lab_2.19.py
@@ -1,12 +1,12 @@
 # Andrew Li
 # 1824794
 
-lemon_cups = float(input("Enter amount of lemon juice (in cups):"))
-water_cups = float(input("Enter amount of water (in cups):"))
-agave_cups = float(input("Enter amount of agave nectar (in cups):"))
+lemon_cups = float(input("Enter amount of lemon juice (in cups):\n"))
+water_cups = float(input("Enter amount of water (in cups):\n"))
+agave_cups = float(input("Enter amount of agave nectar (in cups):\n"))
 num_servings = float(input("How many servings does this make?\n"))
-
+print("\n")
 print("Lemonade ingredients - yields {:.2f} servings".format(num_servings))
-print("{:.2f} cups(s) lemon juice".format(lemon_cups))
-print("{:.2f} cups(s) water".format(water_cups))
-print("{:.2f} cups(s) agave nectar".format(agave_cups))
+print("{:.2f} cup(s) lemon juice".format(lemon_cups))
+print("{:.2f} cup(s) water".format(water_cups))
+print("{:.2f} cup(s) agave nectar".format(agave_cups))
